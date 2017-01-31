68a79f7ebb766281cc9c895034f61c97f6700484
Maciek Kasprzyk <mkasprzyk@szy.fr>
2017-01-31 02:34:39 +0100

    Little fix

--- custom_widget/admin.py
+++ custom_widget/admin.py
@@ -10,11 +10,15 @@ class ConditionsWidget(forms.Widget):
     template_name = 'conditions_widget.html'
 
     def render(self, name, value, attrs=None):
+        try:
+            conditions = json.loads(value)
+        except:
+            conditions = []
         context = {
             'id': attrs.get('id'),
             'name': name,
             'condition_choices': ReactorModel.CONDITION_CHOICES,
-            'conditions': json.loads(value),
+            'conditions': conditions,
         }
         return mark_safe(render_to_string(self.template_name, context))
 
